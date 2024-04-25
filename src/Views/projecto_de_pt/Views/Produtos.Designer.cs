b36e6608f4ac29b46bf0691be172810a437b2ef0
OrlandoMartinho <srsaiombo@gmail.com>
2024-04-25 14:59:58 +0100

    Lutando com tabelas

--- src/Views/projecto_de_pt/Views/Produtos.Designer.cs
+++ src/Views/projecto_de_pt/Views/Produtos.Designer.cs
@@ -28,20 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.guna2GradientPanel1 = new Guna.UI2.WinForms.Guna2GradientPanel();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
+            this.Tabela = new Guna.UI2.WinForms.Guna2DataGridView();
+            this.tb_data = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tb_descricao = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tb_quantidade = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tb_fornecedor = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tb_nome = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tb_preco = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tb_editar = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Tabela)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(18, 37);
+            this.label1.Location = new System.Drawing.Point(17, 37);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(85, 24);
             this.label1.TabIndex = 0;
@@ -61,19 +74,11 @@
             this.label2.Text = "X";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
-            // guna2GradientPanel1
-            // 
-            this.guna2GradientPanel1.Location = new System.Drawing.Point(22, 314);
-            this.guna2GradientPanel1.Name = "guna2GradientPanel1";
-            this.guna2GradientPanel1.ShadowDecoration.Parent = this.guna2GradientPanel1;
-            this.guna2GradientPanel1.Size = new System.Drawing.Size(786, 33);
-            this.guna2GradientPanel1.TabIndex = 3;
-            this.guna2GradientPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2GradientPanel1_Paint);
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(26, 97);
+            this.label3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label3.Location = new System.Drawing.Point(18, 87);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(37, 13);
             this.label3.TabIndex = 4;
@@ -83,7 +88,9 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(129, 97);
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.ForeColor = System.Drawing.Color.Gray;
+            this.label4.Location = new System.Drawing.Point(75, 87);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(87, 13);
             this.label4.TabIndex = 5;
@@ -93,38 +100,206 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(358, 97);
+            this.label5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label5.ForeColor = System.Drawing.Color.Gray;
+            this.label5.Location = new System.Drawing.Point(299, 87);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(51, 13);
             this.label5.TabIndex = 7;
             this.label5.Text = "Consumo";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(271, 97);
+            this.label6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label6.ForeColor = System.Drawing.Color.Gray;
+            this.label6.Location = new System.Drawing.Point(204, 87);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(44, 13);
             this.label6.TabIndex = 6;
             this.label6.Text = "Frescos";
             this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
+            // guna2Button1
+            // 
+            this.guna2Button1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(220)))), ((int)(((byte)(15)))));
+            this.guna2Button1.BorderRadius = 5;
+            this.guna2Button1.BorderThickness = 1;
+            this.guna2Button1.CheckedState.Parent = this.guna2Button1;
+            this.guna2Button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2Button1.CustomImages.Parent = this.guna2Button1;
+            this.guna2Button1.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(220)))), ((int)(((byte)(15)))));
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.ForeColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.FillColor = System.Drawing.Color.White;
+            this.guna2Button1.HoverState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(220)))), ((int)(((byte)(15)))));
+            this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Location = new System.Drawing.Point(545, 87);
+            this.guna2Button1.Name = "guna2Button1";
+            this.guna2Button1.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(219)))), ((int)(((byte)(47)))));
+            this.guna2Button1.ShadowDecoration.Parent = this.guna2Button1;
+            this.guna2Button1.Size = new System.Drawing.Size(108, 29);
+            this.guna2Button1.TabIndex = 8;
+            this.guna2Button1.Text = "Registrar Entrada";
+            // 
+            // guna2Button2
+            // 
+            this.guna2Button2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(220)))), ((int)(((byte)(15)))));
+            this.guna2Button2.BorderRadius = 5;
+            this.guna2Button2.BorderThickness = 1;
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.FillColor = System.Drawing.Color.White;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(253)))), ((int)(((byte)(220)))), ((int)(((byte)(15)))));
+            this.guna2Button2.HoverState.CustomBorderColor = System.Drawing.Color.Black;
+            this.guna2Button2.HoverState.FillColor = System.Drawing.Color.Black;
+            this.guna2Button2.HoverState.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Location = new System.Drawing.Point(697, 87);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.PressedColor = System.Drawing.Color.FromArgb(((int)(((byte)(216)))), ((int)(((byte)(219)))), ((int)(((byte)(47)))));
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(108, 29);
+            this.guna2Button2.TabIndex = 9;
+            this.guna2Button2.Text = "Registrar Saida";
+            // 
+            // Tabela
+            // 
+            this.Tabela.AllowUserToAddRows = false;
+            this.Tabela.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.Tabela.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.Tabela.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Tabela.BackgroundColor = System.Drawing.Color.White;
+            this.Tabela.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.Tabela.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.Tabela.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(208)))), ((int)(((byte)(203)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.Tabela.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.Tabela.ColumnHeadersHeight = 35;
+            this.Tabela.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.Tabela.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.tb_data,
+            this.tb_descricao,
+            this.tb_quantidade,
+            this.tb_fornecedor,
+            this.tb_nome,
+            this.tb_preco,
+            this.tb_editar});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.Tabela.DefaultCellStyle = dataGridViewCellStyle3;
+            this.Tabela.EnableHeadersVisualStyles = false;
+            this.Tabela.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.Tabela.Location = new System.Drawing.Point(21, 132);
+            this.Tabela.Name = "Tabela";
+            this.Tabela.ReadOnly = true;
+            this.Tabela.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.Tabela.RowHeadersVisible = false;
+            this.Tabela.RowHeadersWidth = 30;
+            this.Tabela.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.Tabela.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Tabela.Size = new System.Drawing.Size(784, 249);
+            this.Tabela.TabIndex = 9;
+            this.Tabela.Theme = Guna.UI2.WinForms.Enums.DataGridViewPresetThemes.Default;
+            this.Tabela.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.Tabela.ThemeStyle.AlternatingRowsStyle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F);
+            this.Tabela.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.Tabela.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.Tabela.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.Tabela.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.Tabela.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.Tabela.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(208)))), ((int)(((byte)(203)))));
+            this.Tabela.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.Tabela.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.Tabela.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.Tabela.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.Tabela.ThemeStyle.HeaderStyle.Height = 35;
+            this.Tabela.ThemeStyle.ReadOnly = true;
+            this.Tabela.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.Tabela.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.Tabela.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.Tabela.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.Tabela.ThemeStyle.RowsStyle.Height = 22;
+            this.Tabela.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.Tabela.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // tb_data
+            // 
+            this.tb_data.HeaderText = "Data";
+            this.tb_data.Name = "tb_data";
+            this.tb_data.ReadOnly = true;
+            // 
+            // tb_descricao
+            // 
+            this.tb_descricao.HeaderText = "Descrição";
+            this.tb_descricao.Name = "tb_descricao";
+            this.tb_descricao.ReadOnly = true;
+            // 
+            // tb_quantidade
+            // 
+            this.tb_quantidade.HeaderText = "Quantidade";
+            this.tb_quantidade.Name = "tb_quantidade";
+            this.tb_quantidade.ReadOnly = true;
+            // 
+            // tb_fornecedor
+            // 
+            this.tb_fornecedor.HeaderText = "Fornecedor";
+            this.tb_fornecedor.Name = "tb_fornecedor";
+            this.tb_fornecedor.ReadOnly = true;
+            // 
+            // tb_nome
+            // 
+            this.tb_nome.HeaderText = "Nome";
+            this.tb_nome.Name = "tb_nome";
+            this.tb_nome.ReadOnly = true;
+            // 
+            // tb_preco
+            // 
+            this.tb_preco.HeaderText = "Preço";
+            this.tb_preco.Name = "tb_preco";
+            this.tb_preco.ReadOnly = true;
+            // 
+            // tb_editar
+            // 
+            this.tb_editar.HeaderText = "Editar";
+            this.tb_editar.Name = "tb_editar";
+            this.tb_editar.ReadOnly = true;
+            // 
             // Produtos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(823, 506);
+            this.Controls.Add(this.Tabela);
+            this.Controls.Add(this.guna2Button2);
+            this.Controls.Add(this.guna2Button1);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.guna2GradientPanel1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Produtos";
             this.Text = "Produtos";
             this.Load += new System.EventHandler(this.Produtos_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Tabela)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,11 +309,20 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private Guna.UI2.WinForms.Guna2GradientPanel guna2GradientPanel1;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private Guna.UI2.WinForms.Guna2Button guna2Button1;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
+        private Guna.UI2.WinForms.Guna2DataGridView Tabela;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tb_data;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tb_descricao;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tb_quantidade;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tb_fornecedor;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tb_nome;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tb_preco;
+        private System.Windows.Forms.DataGridViewTextBoxColumn tb_editar;
 
     }
 }
--- src/Views/projecto_de_pt/Views/Produtos.cs
+++ src/Views/projecto_de_pt/Views/Produtos.cs
@@ -5,8 +5,10 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+
 
 namespace projecto_de_pt.Views
 {
@@ -15,7 +17,82 @@ namespace projecto_de_pt.Views
         public Produtos()
         {
             InitializeComponent();
+            PreencherTabela();
+ 
         }
+
+
+        private void PreencherTabela()
+        {
+            // Objeto JSON
+            string json = @"{
+                ""Data"":""20/3/2003"",
+                ""Descricao"":""Entrada"",
+                ""Quantidade"":""12"",
+                ""Fornecedor"":""Orlando"",
+                ""Nome"":""exemplo"",
+                ""Preco"":""20000""
+            }";
+
+            // Desserializa o JSON manualmente
+            Dictionary<string, string> dados = Deserialize(json);
+
+            string caminhoImagem = Path.GetFullPath(Path.Combine(Application.StartupPath, @"assets\icons\teste.png"));
+
+            try
+            {
+                // Carregar a imagem
+                Image imagem = Image.FromFile(caminhoImagem);
+
+                // Criar uma célula com imagem
+                DataGridViewImageCell cell = new DataGridViewImageCell();
+                cell.ImageLayout = DataGridViewImageCellLayout.Zoom; // Definir o layout da imagem
+
+                PictureBox pictureBox = new PictureBox();
+                pictureBox.Image = Image.FromFile(caminhoImagem);
+                pictureBox.SizeMode = PictureBoxSizeMode.Zoom;
+                cell.Value = pictureBox.Image;
+
+
+                // Adicionar os dados à tabela
+                Tabela.Rows.Add(dados["Data"], dados["Descricao"], dados["Quantidade"], dados["Fornecedor"], dados["Nome"], dados["Preco"], pictureBox.Image);
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("Erro: Arquivo de imagem não encontrado. Verifique o caminho da imagem.\n" + ex.Message);
+            }
+            
+
+        }
+
+
+       
+        private Dictionary<string, string> Deserialize(string json)
+        {
+            Dictionary<string, string> dados = new Dictionary<string, string>();
+
+            // Remove os caracteres de inicio e fim
+            json = json.Trim('{', '}');
+
+            // Divide o JSON em pares chave-valor
+            string[] keyValuePairs = json.Split(',');
+
+            foreach (string pair in keyValuePairs)
+            {
+                // Divide o par chave-valor
+                string[] keyValue = pair.Trim().Split(':');
+
+                // Remove aspas dos valores
+                string key = keyValue[0].Trim('"');
+                string value = keyValue[1].Trim('"');
+
+                // Adiciona à lista
+                dados.Add(key, value);
+            }
+
+            return dados;
+        }
+
 
         private void label1_Click(object sender, EventArgs e)
         {
@@ -53,6 +130,16 @@ namespace projecto_de_pt.Views
         }
 
         private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Tabela_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
